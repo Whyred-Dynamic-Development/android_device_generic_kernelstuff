01abb7a40052ab83863489e20e6929f7ae831798
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-03-02 15:22:40 +0530

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 Team Win Recovery Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_kernelstuff.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_kernelstuff-user \
+    twrp_kernelstuff-userdebug \
+    twrp_kernelstuff-eng

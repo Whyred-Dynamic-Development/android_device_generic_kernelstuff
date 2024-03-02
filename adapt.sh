df4eba24300c6f608c547b7528fb607cf0781a38
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-03-02 15:43:22 +0530

    Create adapt.sh

---
+++ adapt.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Default values
+out="Image.gz-dtb"
+pagesize="2048"
+defconfig="vendor/msm8953-perf_defconfig"
+kernelsource="kernel/xiaomi/oxygen"
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --out)
+            out="$2"
+            shift
+            ;;
+        --pagesize)
+            pagesize="$2"
+            shift
+            ;;
+        --defconfig)
+            defconfig="$2"
+            shift
+            ;;
+        --kernel)
+            kernelsource="$2"
+            shift
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# Replace values in BoardConfig.mk
+sed -i "s/^BOARD_KERNEL_IMAGE_NAME := .*$/BOARD_KERNEL_IMAGE_NAME := $out/" device/generic/kernelstuff/BoardConfig.mk
+sed -i "s/^BOARD_KERNEL_PAGESIZE := .*$/BOARD_KERNEL_PAGESIZE := $pagesize/" device/generic/kernelstuff/BoardConfig.mk
+sed -i "s/^TARGET_KERNEL_CONFIG := .*$/TARGET_KERNEL_CONFIG := $defconfig/" device/generic/kernelstuff/BoardConfig.mk
+sed -i "s/^TARGET_KERNEL_SOURCE := .*$/TARGET_KERNEL_SOURCE := $kernelsource/" device/generic/kernelstuff/BoardConfig.mk
